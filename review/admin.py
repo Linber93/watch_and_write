c5a43e3adffb159cb964f1df321ad036994183e4
Linber93 <linus@nybyn.se>
2023-03-08 12:42:57 +0000

    add feature to add reviews from admin panel

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'producer', 'status', 'rating', 'created_on')
+    search_fields = ['title', 'producer']
+    summernote_fields = ('opinion', 'conclusion')
