25e583c1a47c9fd3c3d9543440572a35c1fee903
Linber93 <linus@nybyn.se>
2023-03-13 20:20:13 +0000

    add urlpattern for edit and delete reviews

--- review/urls.py
+++ review/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.LandingPage.as_view(), name='home'),
     path('directory/', views.ReviewList.as_view(), name='directory'),
     path('create-review/', views.CreateReview.as_view(), name='create_review'),
+    path('reviews/edit/<slug:slug>/', views.EditReview.as_view(), name='edit_review'),
+    path('reviews/<slug:slug>/remove', views.DeleteReview.as_view(), name='delete_review'),
     path('<slug:slug>/', views.Review_Single.as_view(), name='single_review'),
+
 ]
